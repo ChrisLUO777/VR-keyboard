850e7c6b77b7df9c35f86e39a3307c9c737000bf
LUOShiqing <2386004278@qq.com>
2020-09-02 01:07:31 -0400

    Added one key and one sim controller.

---
+++ Assets/Scripts/SimController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimController : MonoBehaviour
+{
+    public Vector3 lastPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            lastPosition = new Vector3(lastPosition.x, lastPosition.y + 0.1f, lastPosition.z);
+            transform.position = lastPosition;
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            lastPosition = new Vector3(lastPosition.x, lastPosition.y - 0.1f, lastPosition.z);
+            transform.position = lastPosition;
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            lastPosition = new Vector3(lastPosition.x - 0.1f, lastPosition.y, lastPosition.z);
+            transform.position = lastPosition;
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            lastPosition = new Vector3(lastPosition.x + 0.1f, lastPosition.y, lastPosition.z);
+            transform.position = lastPosition;
+        }
+    }
+}
